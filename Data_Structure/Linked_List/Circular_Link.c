13c66f8f0d992e5d53e03a2f20d978b3b9e3c7f0
ian0813 <hippo032318@gmail.com>
2019-10-09 13:10:24 +0800

    add new

---
+++ Data_Structure/Linked_List/Circular_Link.c
@@ -0,0 +1,62 @@
+
+
+    #include<stdio.h>
+    #include<time.h>
+    #include<stdlib.h>
+
+    struct node{
+        int data;
+        struct node *next;
+    };
+
+    struct node* create(struct node *n)
+    {
+         n = (struct node*) malloc(sizeof(struct node));
+         n->data = rand()%100;
+         n->next = n;
+         return n;
+    }    
+
+    void generate(struct node *n)
+    {
+        struct node *temp, *new;
+        new = (struct node*) malloc(sizeof(struct node));
+        new->data = rand()%100;
+        new->next = NULL;
+        temp = n;
+        while(temp->next != n)
+            temp = temp->next;
+        temp->next = new;
+        new->next = n;
+    }
+
+    void traverse(struct node *n)
+    {
+        struct node *temp;
+        temp = n;
+        while(temp->next != n)
+            printf("%d  ", temp->data);
+        printf("%d  \n", temp->data);
+    }
+
+    int main()
+    {
+        struct node *head;
+
+        srand(time(NULL));
+        int amount;
+        head = create(head);
+        traverse(head);
+        printf("Please enter a number so decide the length of link: \n");
+        scanf("%d", &amount);
+
+        while(amount > 0)
+        {
+            generate(head);
+            amount--;
+        }    
+
+        traverse(head);
+
+        return 0;
+    }
