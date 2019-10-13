590ef5efc37812c256f4789d7b9b0d6631056e36
Ian0813 <42731370+Ian0813@users.noreply.github.com>
2019-10-13 13:27:11 +0800

    Update Double_Link.c

--- Data_Structure/Linked_List/Double_Link.c
+++ Data_Structure/Linked_List/Double_Link.c
@@ -10,9 +10,16 @@
 
             head = create();
 
-            int action;
+            int action, amount;
             srand(time(NULL));
-
+            printf("Please enter number od amount : ");
+            scanf("%d", &amount);
+            while(amount != 0)
+            {
+                generate(head, rand()%100);
+                amount--;
+            }
+            
             while(1)
             {
                 printf("For what you want to do, please enter a number. \n 1.Insert \n 2.Delete \n 3.Traverse \n");
@@ -20,13 +27,11 @@
                 fflush(stdin);
                 
                 if(action == 1)
-                    insert(head);
+                    insert(head, rand()%100);
                 else if(action == 2)
                     head = delete(head);
                 else if(action == 3)
                     traverse(head);
                 
-            }
-            
-             
+            }        
         }
